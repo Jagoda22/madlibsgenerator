9b3cbd11be382e703e2a54c533734bb4a19e09a9
Jagoda <jrutkowska7@st.swps.edu.pl>
2021-03-10 20:54:03 +0100

    project

---
+++ proyecto.py
@@ -0,0 +1,67 @@
+print("""Hello in our mad libs game!
+To start our game, help me define following gaps:
+
+""")
+
+
+male = input("Male Name: ")
+teacher = input("Name of your favorite teacher: ")
+exc = input("Exclamation: ")
+num = input("Number: ")
+plur = input("Plural Objects: ")
+stor = input("Store Name: ")
+body = input("Body Part: ")
+word = input("Silly Word: ")
+hol = input("Name of Holiday: ")
+celeb = input("Celebrity Name: ")
+movie = input("Movie Title: ")
+ending = input("Verb Ending in -Ing: ")
+dist = input("Amount of Distance: ")
+count = input("Country: ")
+obj = input("Object: ")
+occ = input("Occupation: ")
+ani = input("Animal: ")
+movie = input("Famous Movie Quote: ")
+body2 = input("Body Part: ")
+child = input("Children’s Song: ")
+adj = input("Adjective: ")
+
+print("""Lets have fun now ;) 
+That's what you've created.""")
+
+
+print(f"""
+
+DETECTIVE: Hello. I’m Detective {male}. And, you are…
+SUSPECT: {teacher}  
+DETECTIVE: You are here today under suspicion of second degree robbery.
+SUSPECT: {exc}!  
+DETECTIVE That’s right. {num} {plur} were stolen from {stor} and the crime scene has your {body} written ALL over it.
+SUSPECT: That is {word}.
+DETECTIVE: Where were you on the night of {hol}?
+SUSPECT: I was with {celeb}. We were watching the movie {movie}.
+DETECTIVE: Then why does security camera footage show you {ending} just {dist} away from the crime scene?
+SUSPECT: That’s a coincidence.
+DETECTIVE: Alright, I’m through playing games. Where are you from?SUSPECT: {count}.
+DETECTIVE: What did your parents do for a living?
+SUSPECT: My father was a {obj} salesman and my mother was a {occ}.
+DETECTIVE: Did they teach you to steal?
+SUSPECT: No. I told you didn’t do it!
+DETECTIVE: That’s enough! One of the best parts about being a detective is that I get to lock up criminals like you, and go home to my children and my pet [Animal] and say, [Famous Movie Quote].
+SUSPECT: Fine, I did it. I committed the robbery. But I only did it because I needed the money to buy myself {body2} implants.
+DETECTIVE: I knew it all along. And every time I solve a crime, I sing my favorite song, {child}
+
+SUSPECT: You have a {adj} voice.
+(BEAT) """)
+
+
+
+
+
+
+
+
+
+
+
+
